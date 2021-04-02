b775533518b43d98e09733cd0ddc82814fc6466a
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-04-02 16:23:17 +0600

    text template

---
+++ goTemplate/textTemplate/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"os"
+)
+
+func main() {
+	emailtext := `hello {{.firstName}} {{.lastName}}
+	Thank you for registering at myapp. 
+	your login url {{.url}}
+	Thank you so much`
+
+	data := struct {
+		firstName string
+		lastName  string
+		url       string
+	}{
+		"Md Abu",
+		"Raihan",
+		"facebook.com/raihan.mahmudi.50",
+	}
+	tpl := template.New("emltmp")
+	eml, err := tpl.Parse(emailtext)
+	if err != nil {
+		fmt.Println(err)
+	}
+	eml.Execute(os.Stdout, data)
+
+}
