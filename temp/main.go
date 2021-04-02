d381fd8e37cca508a44ca99947680f28c65bb8f0
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-04-02 11:41:54 +0600

    first html website print

---
+++ temp/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Md Abu Raihan"
+	heading := "This website heading one"
+	paragraph := "this is my website first paragraph"
+	webtem := `
+	<!DOCTYPE html>
+	<html>
+	<head>
+	<title>` + name + `</title>
+	</head>
+	<body>
+	
+	<h1>` + heading + `</h1>
+	<p>` + paragraph + `</p>
+	
+	</body>
+	</html> 
+	`
+	fmt.Println(webtem)
+}
