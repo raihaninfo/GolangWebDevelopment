11024c544bd823dda974b8101f4d7a0fdd4188ce
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-04-04 15:05:42 +0600

    create html template

---
+++ goTemplate/htmlTemplate/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"text/template"
+)
+
+func main() {
+	// template parse
+	tmp, err := template.ParseFiles("one.gohtml", "two.gohtml")
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	// template exicute
+	tmp.Execute(os.Stdout, nil)
+}
