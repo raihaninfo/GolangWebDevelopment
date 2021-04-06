566eaa897e154f472eaa3f74ef0a86bba0638b51
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-04-06 10:31:16 +0600

    create http server

---
+++ httpPackage/httpServer/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "net/http"
+
+func main() {
+	// route
+	http.HandleFunc("/", home)
+	// http server
+	http.ListenAndServe(":8080", nil)
+}
+
+// route function
+func home(w http.ResponseWriter, r *http.Request) {
+	// response
+}
