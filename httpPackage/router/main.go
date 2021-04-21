9396c8d680a60a51232065a55356ad8f4512b672
raihaninfo <raihanmahmudi35@gmail.com>
2021-04-21 11:57:59 +0600

    add 404 page

--- httpPackage/router/main.go
+++ httpPackage/router/main.go
@@ -6,10 +6,10 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", home)
+	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
 }
-func home(w http.ResponseWriter, r *http.Request) {
+func handler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	if r.URL.Path == "/" {
 		fmt.Fprint(w, "<h2>This is home page</h2>")
@@ -17,6 +17,8 @@ func home(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "<h2>This is About page</h2>")
 	} else if r.URL.Path == "/con" {
 		fmt.Fprint(w, "<h2>Contact page</h2>")
+	} else {
+		fmt.Fprint(w, "<h2>This is 404 page</h2>")
 	}
 
 }
