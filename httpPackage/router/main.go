1c6f1d7071591059979a39c595364457ffb2d2e3
raihaninfo <raihanmahmudi35@gmail.com>
2021-04-22 15:20:49 +0600

    template added

--- httpPackage/router/main.go
+++ httpPackage/router/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
@@ -12,13 +13,33 @@ func main() {
 func handler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h2>This is home page</h2>")
+		// fmt.Fprint(w, "<h2>This is home page</h2>")
+		tmp, err := template.ParseFiles("index.gohtml")
+
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+
+		// template exicute
+		tmp.Execute(w, nil)
+
 	} else if r.URL.Path == "/about" {
-		fmt.Fprint(w, "<h2>This is About page</h2>")
+		// fmt.Fprint(w, "<h2>This is About page</h2>")
+		tmp, err := template.ParseFiles("about.gohtml")
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		tmp.Execute(w, nil)
 	} else if r.URL.Path == "/con" {
 		fmt.Fprint(w, "<h2>Contact page</h2>")
 	} else {
-		fmt.Fprint(w, "<h2>This is 404 page</h2>")
+		// fmt.Fprint(w, "<h2>This is 404 page</h2>")
+		w.WriteHeader(http.StatusNotFound)
+		tmp, err := template.ParseFiles("404.gohtml")
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		tmp.Execute(w, nil)
 	}
 
 }
