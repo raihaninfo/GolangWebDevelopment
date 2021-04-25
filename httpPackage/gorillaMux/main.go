c473a039f1b87a4de5b27c4033d0ce9d35359902
raihaninfo <raihanmahmudi35@gmail.com>
2021-04-26 00:55:58 +0600

    add sum text

--- httpPackage/gorillaMux/main.go
+++ httpPackage/gorillaMux/main.go
@@ -14,11 +14,12 @@ func main() {
 	r.HandleFunc("/about", about)
 	http.ListenAndServe(":8080", r)
 }
+
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h2>This is home page </h2>")
 }
 func about(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>This is about page </h1>")
+	fmt.Fprint(w, "<h1>This is About page </h1>")
 }
