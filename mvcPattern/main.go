04cf364a676cd707eb43fd54703d10ed47b5d126
raihaninfo <raihanmahmudi35@gmail.com>
2021-05-04 12:14:52 +0600

    add about page and route

--- mvcPattern/main.go
+++ mvcPattern/main.go
@@ -10,28 +10,40 @@ import (
 var (
 	homeView    *views.View
 	contactView *views.View
+	aboutView   *views.View
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	err:= homeView.Template.Execute(w, nil)
-	if err != nil{
+	err := homeView.Template.Execute(w, nil)
+	if err != nil {
 		panic(err)
 	}
 }
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	err:= contactView.Template.Execute(w, nil)
-	if err!= nil {
+	err := contactView.Template.Execute(w, nil)
+	if err != nil {
 		panic(err)
 	}
 }
+
+func about(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	err := aboutView.Template.Execute(w, nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
 func main() {
 	homeView = views.NewView("views/home.gohtml")
 	contactView = views.NewView("views/contact.gohtml")
+	aboutView = views.NewView("views/about.gohtml")
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/about", about)
 	http.ListenAndServe(":8080", r)
 }
