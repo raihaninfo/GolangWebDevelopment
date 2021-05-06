d463a62077c8bd6ad2584d8b3c2a0608be5cc80d
raihaninfo <raihanmahmudi35@gmail.com>
2021-05-06 12:06:08 +0600

    Cleaned up our view.go code a bit

--- mvcPattern/views/views.go
+++ mvcPattern/views/views.go
@@ -3,14 +3,16 @@ package views
 import (
 	"fmt"
 	"html/template"
+	"path/filepath"
+)
+
+var (
+	LayoutDir   string = "views/layouts/"
+	TemplateExt string = ".gohtml"
 )
 
 func NewView(layout string, files ...string) *View {
-	files = append(files,
-		"views/layouts/header.gohtml",
-		"views/layouts/navbar.gohtml",
-		"views/layouts/footer.gohtml",
-	)
+	files = append(files, layoutFiles()...)
 	t, err := template.ParseFiles(files...)
 	if err != nil {
 		fmt.Println(err.Error())
@@ -25,4 +27,12 @@ func NewView(layout string, files ...string) *View {
 type View struct {
 	Template *template.Template
 	Layout   string
+}
+
+func layoutFiles() []string {
+	files, err := filepath.Glob(LayoutDir + "*" + TemplateExt)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	return files
 }
