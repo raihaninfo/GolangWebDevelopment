4eda4dbbf4e899ccf81e3fef742ec6953fa0e44d
raihaninfo <raihanmahmudi35@gmail.com>
2021-05-06 15:46:47 +0600

    add singup and login page

--- mvcPattern/main.go
+++ mvcPattern/main.go
@@ -11,6 +11,8 @@ var (
 	homeView    *views.View
 	contactView *views.View
 	aboutView   *views.View
+	singupView  *views.View
+	loginView   *views.View
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -24,21 +26,32 @@ func contact(w http.ResponseWriter, r *http.Request) {
 
 func about(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	err := aboutView.Template.ExecuteTemplate(w, aboutView.Layout, nil)
-	if err != nil {
-		panic(err)
-	}
+	chErr(aboutView.Render(w, nil))
+}
+
+func singup(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	chErr(singupView.Render(w, nil))
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	chErr(loginView.Render(w, nil))
 }
 
 func main() {
 	homeView = views.NewView("header", "views/home.gohtml")
 	contactView = views.NewView("header", "views/contact.gohtml")
 	aboutView = views.NewView("header", "views/about.gohtml")
+	singupView = views.NewView("header", "views/singup.gohtml")
+	loginView = views.NewView("header", "views/login.gohtml")
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
 	r.HandleFunc("/about", about)
+	r.HandleFunc("/singup", singup)
+	r.HandleFunc("/login", login)
 	http.ListenAndServe(":8080", r)
 }
 
