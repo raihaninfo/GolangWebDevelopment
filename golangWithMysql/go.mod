f7896c21e6559f02bd75bee27bbc5a3885c18e28
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-09-22 19:46:37 +0600

    connect database

---
+++ golangWithMysql/go.mod
@@ -0,0 +1,5 @@
+module github.com/raihaninfo/GolangWebDevelopment
+
+go 1.17
+
+require github.com/go-sql-driver/mysql v1.6.0
--- golangWithMysql/main.go
+++ golangWithMysql/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
 
 func main() {
-	fmt.Println("Hi...")
+
+	db, err := sql.Open("mysql", "root:mysql@tcp(127.0.0.1:3306)/sql-golang")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Connected to the database....")
+	defer db.Close()
 }
