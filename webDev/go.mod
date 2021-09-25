5bd49e300e0e4c563cc9ee534f92b8bfefbb2cc9
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-09-25 14:08:41 +0600

    perse data in html

---
+++ webDev/go.mod
@@ -0,0 +1,3 @@
+module GolangWebDevelopment/webDev
+
+go 1.17
---
+++ webDev/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"text/template"
+)
+
+type myinfo struct {
+	Name       string
+	Age        int
+	FatherName string
+	Pession    string
+	Mobile     string
+}
+
+var name string = "Hasan"
+var info myinfo
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	tem, err := template.ParseFiles("templates/home.gohtml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	tem.Execute(w, info)
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	tem, err := template.ParseFiles("templates/about.gohtml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	tem.Execute(w, nil)
+}
+
+func main() {
+	info = myinfo{
+		Name:       "Md Abu Raihan",
+		Age:        21,
+		FatherName: "Robiul Islam",
+		Pession:    "Programming",
+		Mobile:     "01853566901",
+	}
+
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/about", aboutHandler)
+	http.ListenAndServe(":8080", nil)
+}
