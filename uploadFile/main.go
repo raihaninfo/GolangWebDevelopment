8b815b8456a2d7784d7313a9b0da7bb7f49756df
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-09-30 14:50:20 +0600

    router and file path change

--- uploadFile/main.go
+++ uploadFile/main.go
@@ -10,25 +10,25 @@ import (
 
 // }
 
-// func actionHandler(w http.ResponseWriter, r *http.Request) {
-// 	tmp, err := template.ParseFiles("template/action.gohtml")
-// 	if err != nil {
-// 		fmt.Println(err.Error())
-// 	}
-// 	tmp.Execute(w, nil)
-// }
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	tmp, err := template.ParseFiles("template/home.gohtml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	tmp.Execute(w, nil)
+}
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/upload", func(w http.ResponseWriter, r *http.Request) {
 		// fmt.Println(r.Method)
-		tmp, err := template.ParseFiles("template/home.gohtml")
+		tmp, err := template.ParseFiles("template/upload.gohtml")
 		if err != nil {
 			fmt.Println(err.Error())
 		}
 		tmp.Execute(w, nil)
 	})
 
-	// http.HandleFunc("/action", actionHandler)
+	http.HandleFunc("/", homeHandler)
 
 	http.ListenAndServe(":8080", nil)
 }
