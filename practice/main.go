7aeeb3772a580b9ac8cc2a52c7f05909048f2f1b
raihaninfo <raihanmahmudi35@gmail.com>
2021-11-12 22:53:30 +0600

    create login page and login handler

--- practice/main.go
+++ practice/main.go
@@ -16,6 +16,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 	}
 	tmp.Execute(w, nil)
 }
+
 func notFount(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNotFound)
 	tmp, err := template.ParseFiles("view/404.gohtml", "view/header.gohtml")
@@ -33,10 +34,19 @@ func about(w http.ResponseWriter, r *http.Request) {
 	tmp.Execute(w, nil)
 }
 
+func login(w http.ResponseWriter, r *http.Request) {
+	tmp, err := template.ParseFiles("view/login.gohtml", "view/header.gohtml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	tmp.Execute(w, nil)
+}
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/about", about)
+	r.HandleFunc("/login", login)
 	r.NotFoundHandler = http.HandlerFunc(notFount)
 	fmt.Println("Listening port :8080")
 	http.ListenAndServe(":8080", r)
