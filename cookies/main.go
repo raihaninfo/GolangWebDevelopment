3b908d30f13b81e869f95304add105c2f0b16282
raihaninfo <raihanmahmudi35@gmail.com>
2021-11-14 15:24:40 +0600

    session create and delete practiced

--- cookies/main.go
+++ cookies/main.go
@@ -4,16 +4,62 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
 )
 
-func main() {
+var store = sessions.NewCookieStore([]byte("secret-password"))
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		tmp, err := template.ParseFiles("templates/home.gohtml")
-		if err != nil {
-			fmt.Println(err.Error())
-		}
-		tmp.Execute(w, nil)
-	})
-	http.ListenAndServe(":8080", nil)
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/create", create)
+	r.HandleFunc("/delete", delete)
+	fmt.Println("Listening port :8080")
+	http.ListenAndServe(":8080", r)
+}
+
+func create(w http.ResponseWriter, r *http.Request) {
+	session, err := store.Get(r, "login-session")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	session.Options = &sessions.Options{
+		Path:     "/",
+		MaxAge:   86400 * 7,
+		HttpOnly: true,
+	}
+	name := "Raihan"
+	session.Values["name"] = name
+	err = session.Save(r, w)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	tpl, err := template.ParseFiles("templates/create.gohtml")
+	if err != nil {
+		panic(err)
+	}
+	tpl.Execute(w, nil)
+}
+
+func delete(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "login-session")
+	session.Options.MaxAge = -1
+	session.Save(r, w)
+	tpl, err := template.ParseFiles("templates/delete.gohtml")
+	if err != nil {
+		panic(err)
+	}
+	tpl.Execute(w, nil)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	temp, err := template.ParseFiles("templates/home.gohtml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	temp.Execute(w, nil)
 }
