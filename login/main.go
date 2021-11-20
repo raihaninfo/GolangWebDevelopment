6516a71be279581d48490788c8881e955791e6b8
raihaninfo <raihanmahmudi35@gmail.com>
2021-11-20 14:18:53 +0600

    complete loging system done

--- login/main.go
+++ login/main.go
@@ -15,9 +15,10 @@ func main() {
 	r := mux.NewRouter()
 	r.NotFoundHandler = http.HandlerFunc(notFount)
 	r.HandleFunc("/", home)
+	r.HandleFunc("/about", about)
 	r.HandleFunc("/login", login)
 	r.HandleFunc("/loginauth", loginAuth)
-	// r.HandleFunc("/logout", logout)
+	r.HandleFunc("/logout", logout)
 	fmt.Println("Listening port :8080")
 	http.ListenAndServe(":8080", r)
 }
@@ -31,6 +32,12 @@ func notFount(w http.ResponseWriter, r *http.Request) {
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "login-session")
+	_, ok := session.Values["username"]
+	if !ok {
+		http.Redirect(w, r, "/login", http.StatusFound)
+		return
+	}
 	tem, err := template.ParseFiles("temp/home.gohtml", "temp/header.gohtml")
 	if err != nil {
 		fmt.Println(err.Error())
@@ -38,12 +45,40 @@ func home(w http.ResponseWriter, r *http.Request) {
 	tem.Execute(w, nil)
 }
 
-func login(w http.ResponseWriter, r *http.Request) {
-	tem, err := template.ParseFiles("temp/login.gohtml", "temp/header.gohtml")
+func about(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "login-session")
+	_, ok := session.Values["username"]
+	if !ok {
+		http.Redirect(w, r, "/login", http.StatusFound)
+		return
+	}
+	tem, err := template.ParseFiles("temp/about.gohtml", "temp/header.gohtml")
 	if err != nil {
-		panic(err)
+		fmt.Println(err.Error())
 	}
 	tem.Execute(w, nil)
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "login-session")
+	_, ok := session.Values["username"]
+	if ok {
+		http.Redirect(w, r, "/", http.StatusFound)
+		return
+	}
+	tem, err := template.ParseFiles("temp/login.gohtml", "temp/header.gohtml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	tem.Execute(w, nil)
+
+}
+
+func logout(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "login-session")
+	delete(session.Values, "username")
+	session.Save(r, w)
+	http.Redirect(w, r, "/login", http.StatusFound)
 }
 
 func loginAuth(w http.ResponseWriter, r *http.Request) {
@@ -78,17 +113,13 @@ func loginAuth(w http.ResponseWriter, r *http.Request) {
 			MaxAge:   86400 * 30,
 			HttpOnly: true,
 		}
-		session.Values["name"] = username
+		session.Values["username"] = username
 		err = session.Save(r, w)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-		temp, err := template.ParseFiles("temp/loginauth.gohtml", "temp/header.gohtml")
-		if err != nil {
-			panic(err)
-		}
-		temp.Execute(w, nil)
+		http.Redirect(w, r, "/", http.StatusFound)
 
 	}
 
